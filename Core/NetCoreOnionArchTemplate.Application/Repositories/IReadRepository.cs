ec4d08745814602aa20191009bd300a6a2cbbafb
safauludogan <safauludogan@kamutech.com.tr>
2023-12-11 14:51:11 +0300

    tracking optimization for IReadRepository

--- Core/NetCoreOnionArchTemplate.Application/Repositories/IReadRepository.cs
+++ Core/NetCoreOnionArchTemplate.Application/Repositories/IReadRepository.cs
@@ -5,9 +5,9 @@ namespace NetCoreOnionArchTemplate.Application.Repositories
 {
     public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
     {
-        IQueryable<T> GetAll();
-        IQueryable<T> GetWhere(Expression<Func<T, bool>> method);
-        Task<T> GetSingleAsync(Expression<Func<T, bool>> method);
-        Task<T> GetByIdAsync(int Id);
+        IQueryable<T> GetAll(bool tracking = true);
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true);
+        Task<T> GetByIdAsync(int Id, bool tracking = true);
     }
 }
