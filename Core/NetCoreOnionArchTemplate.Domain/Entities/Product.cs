70126245442dbc0836eae2180372df375b91c0f9
safauludogan <safauludogan@kamutech.com.tr>
2023-12-11 14:50:19 +0300

    Ppdated ProductEntity

--- Core/NetCoreOnionArchTemplate.Domain/Entities/Product.cs
+++ Core/NetCoreOnionArchTemplate.Domain/Entities/Product.cs
@@ -5,7 +5,7 @@ namespace NetCoreOnionArchTemplate.Domain.Entities
     public class Product : BaseEntity
     {
         public string Name { get; set; }
-        public int Price { get; set; }
+        public float Price { get; set; }
         public int Stock { get; set; }
         public ICollection<Order> Orders { get; set; }
     }
