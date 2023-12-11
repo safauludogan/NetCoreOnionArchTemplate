81bdeae312fdb35b3a7fcd0ba7c5351d277c0f73
safauludogan <safauludogan@kamutech.com.tr>
2023-12-11 12:01:56 +0300

    Tracking Performance Optimization on readrepository

--- Infrastructure/NetCoreOnionArchTemplate.Persistence/Repositories/ReadRepository.cs
+++ Infrastructure/NetCoreOnionArchTemplate.Persistence/Repositories/ReadRepository.cs
@@ -17,18 +17,38 @@ namespace NetCoreOnionArchTemplate.Persistence.Repositories
 
         public DbSet<T> Table => _context.Set<T>();
 
-        public IQueryable<T> GetAll()
-          => Table;
+        public IQueryable<T> GetAll(bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
 
-        public async Task<T> GetByIdAsync(int Id)
-         => await Table.Where(x => x.Id == Id).FirstOrDefaultAsync();
+        public async Task<T> GetByIdAsync(int Id, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await query.Where(x => x.Id == Id).FirstOrDefaultAsync();
+        }
 
 
-        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
-         => await Table.FirstOrDefaultAsync(method);
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await query.FirstOrDefaultAsync(method);
+        }
 
-        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
-        => Table.Where(method);
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            var query = Table.Where(method);
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
 
     }
 }
