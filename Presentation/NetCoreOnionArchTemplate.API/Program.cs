59f151184efe9d56fe1d5cb0ed9d2d4a305bfeea
safauludogan <safauludogan@kamutech.com.tr>
2023-12-12 11:41:26 +0300

    cors policy added

--- Presentation/NetCoreOnionArchTemplate.API/Program.cs
+++ Presentation/NetCoreOnionArchTemplate.API/Program.cs
@@ -4,6 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPersistanceServices();
 
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+//policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()
+policy.WithOrigins("http://localhost.7285", "https://localhost.7285").AllowAnyHeader().AllowAnyMethod()
+));
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
