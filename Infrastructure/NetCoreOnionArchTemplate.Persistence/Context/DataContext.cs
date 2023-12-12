175ead5899b12afc168b86c19e5ea52fce6a44cd
safauludogan <safauludogan@kamutech.com.tr>
2023-12-12 10:09:42 +0300

    SaveChangeAsync Interceptor added

--- Infrastructure/NetCoreOnionArchTemplate.Persistence/Context/DataContext.cs
+++ Infrastructure/NetCoreOnionArchTemplate.Persistence/Context/DataContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NetCoreOnionArchTemplate.Domain.Entities;
+using NetCoreOnionArchTemplate.Domain.Entities.Common;
 
 namespace NetCoreOnionArchTemplate.Persistence.Context
 {
@@ -11,5 +12,25 @@ namespace NetCoreOnionArchTemplate.Persistence.Context
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+
+            //ChangeTracker : Entityler üzerinden yapılan değişikliklerin ya da yeni eklenen verinin yakalanmasını sağlayan property. Update operasyonlarında Track edilen verileri yakalar.
+
+            var datas = ChangeTracker
+                .Entries<BaseEntity>();
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.Now,
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.Now
+                };
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
