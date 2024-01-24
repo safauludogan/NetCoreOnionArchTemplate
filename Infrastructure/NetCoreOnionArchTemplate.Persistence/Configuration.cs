0909f63cd721fc8cb052cbee757b774f275c45a6
safauludogan <suludogan95@gmail.com>
2024-01-24 15:00:38 +0300

    Fix in Configuration.

--- Infrastructure/NetCoreOnionArchTemplate.Persistence/Configuration.cs
+++ Infrastructure/NetCoreOnionArchTemplate.Persistence/Configuration.cs
@@ -2,20 +2,27 @@
 
 namespace NetCoreOnionArchTemplate.Persistence
 {
-    static class Configuration
-    {
-        static public string ConnectionString
-        {
-            get
-            {
-                //Microsoft.Extensions.Configuration alttaki manager için yüklenmesi gerekiyor.
-                ConfigurationManager configurationManager = new();
-                //appsettings.json'ın path'ini burada ayarlamamız gerekiyor.
-                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/NetCoreOnionArchTemplate.API"));
-                //Microsoft.Extensions.Configuration.Json alttaki json_reader için yüklenmesi gerekiyor
-                configurationManager.AddJsonFile("appsettings.json");
-                return configurationManager.GetConnectionString("DefaultConnection");
-            }
-        }
-    }
+	static class Configuration
+	{
+		static public string ConnectionString
+		{
+			get
+			{
+				//Microsoft.Extensions.Configuration alttaki manager için yüklenmesi gerekiyor.
+				ConfigurationManager configurationManager = new();
+				try
+				{
+					//appsettings.json'ın path'ini burada ayarlamamız gerekiyor.
+					configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/NetCoreOnionArchTemplate.API"));
+					//Microsoft.Extensions.Configuration.Json alttaki json_reader için yüklenmesi gerekiyor
+					configurationManager.AddJsonFile("appsettings.json");
+					return configurationManager.GetConnectionString("DefaultConnection");
+				}
+				catch
+				{
+					return configurationManager.GetConnectionString("DefaultConnection");
+				}
+			}
+		}
+	}
 }
